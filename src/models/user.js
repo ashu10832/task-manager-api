dae3d385eb94a101c0b18e8936848abf16ce8457
Ashu Gupta <ashu10832@gmail.com>
2020-06-10 17:42:57 +0530

    fixed bug

--- src/models/user.js
+++ src/models/user.js
@@ -105,7 +105,7 @@ userSchema.statics.findByCredentials = async (email,password) =>{
 
 userSchema.methods.generateAuthToken = async function(){
     const user = this
-    const token = await jwt.sign({_id:user._id.toString()},JWT_SECRET,{
+    const token = await jwt.sign({_id:user._id.toString()},process.env.JWT_SECRET,{
         expiresIn:'7 days'
     })
     user.tokens = user.tokens.concat({token})
